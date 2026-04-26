3b1e3e6d7732c27309d3a3e7569c684c26bbdb94
bstahlman27 <bstahlman27@gmail.com>
2026-04-26 12:15:27 -0400

    Contact form works

--- assets/scripts/formSubmission.js
+++ assets/scripts/formSubmission.js
@@ -1,16 +1,20 @@
-emailjs.init('PUBLIC KEY');
+if (!window._formInit){
+    window._formInit = true;
 
-document.querySelector('.contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const btn = document.querySelector('.form-submit');
-    btn.textContent = 'Sending...';
-    btn.disabled = true;
+    emailjs.init('_SIgm4WmwcPS0GGKV');
 
-    emailjs.sendForm('SERVICE ID', 'TEMPLATE ID', this).then(() => {
-        btn.textContent = 'Sent!';
-        this.reset();
-    }).catch(() => {
-        btn.textContent = 'Failed — Try Again';
-        btn.disabled = false;
+    document.querySelector('.contact-form').addEventListener('submit', function(e) {
+        e.preventDefault();
+        const btn = document.querySelector('.form-submit');
+        btn.textContent = 'Sending...';
+        btn.disabled = true;
+
+        emailjs.sendForm('nuway-site-contact-form', 'template_fg1f5v7', this).then(() => {
+            btn.textContent = 'Sent!';
+            this.reset();
+        }).catch(() => {
+            btn.textContent = 'Failed — Try Again';
+            btn.disabled = false;
+        });
     });
-});
+}
